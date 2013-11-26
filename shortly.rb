ef16fd79e082753f8931beef35549bdb1a9dc50f
Geoff Crittenden <geoffcritt@hotmail.com>
2013-11-26 15:42:02 -0600

    using object_id as url identifier

--- shortly.rb
+++ shortly.rb
@@ -1,11 +1,17 @@
 class Shortly
 
+  attr_reader :shortly, :longly
+
   def initialize(url)
-    url.to_i(36)
-    
+    puts self.object_id
+    @shortly = "http://short.ly/#{self.object_id.to_s(36)}"
+    @longly  = url
+
   end
 
 end
 
 url = "http://www.citypulse.io"
 short = Shortly.new(url)
+puts short.shortly
+puts short.longly
