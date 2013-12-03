14fff799d8e43f0792c448f08fa96512ea167afd
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-02 21:16:27 -0600

    added ENV['RACK_ENV'] environment variable

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 # See: http://gembundler.com/bundler_setup.html
 #      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+ENV['RACK_ENV'] = :production
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
