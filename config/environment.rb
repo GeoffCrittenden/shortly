d0f8d819469f31fffe0fcb968229e4a8d926ca25
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-06 12:44:08 -0600

    fixed environment.rb after removing New Relic

--- config/environment.rb
+++ config/environment.rb
@@ -20,9 +20,6 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
-require 'newrelic_rpm'
-ENV['RACK_ENV'] ||= production
-
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
